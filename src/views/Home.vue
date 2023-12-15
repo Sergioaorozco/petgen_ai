784342f7558b51dbc4b428b59c0866a0effd6f1f
Sergio <sergioa.orozcoo@gmail.com>
2023-12-15 09:44:17 -0500

    Changes in Animals to improve performance

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex justify-center items-center h-full">
     <main class="text-center">
-        <h1 class="text-5xl font-bold text-slate-600 my-5">Cadáver Exquisito</h1>
-        <h2 class="text-slate-500 text-2xl">Generate funny and catchy names for your pet using a multi Step platform</h2>
+        <h1 class="text-5xl font-bold text-slate-600 my-5">Exquisit Corpse</h1>
+        <h2 class="text-slate-500 text-2xl">Generate funny and catchy names for your pet using a multi Step platform and AI</h2>
         <router-link to="/step1"><button class="px-10 py-2 bg-amber-500 my-10 rounded-lg text-xl hover:bg-amber-500/70 transition-colors font-semibold duration-200">Start Generating</button></router-link>
     </main>
   </div>
